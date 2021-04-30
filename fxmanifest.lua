3a455bfe443f98ae86e6c8e0676ab9572493b9c4
NezOnToast <81484877+NezOnToast@users.noreply.github.com>
2021-04-30 18:18:01 +0100

    Added files

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Nez'
+description 'Badger Report System ReWrite [Reports+]'
+version '1.1.0'
+
+client_script 'client.lua'
+shared_script 'config.lua'
+server_script 'server.lua'
